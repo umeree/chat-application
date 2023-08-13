5e05e8d7974a77d48b35988999cdeede4c99df02
umeree <umernadeam@gmail.com>
2023-08-13 16:39:02 +0500

    responsive

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDZHqzPE4cUfTIcUJs6rWoBQNG0DFGvFao",
+  authDomain: "chat-93135.firebaseapp.com",
+  projectId: "chat-93135",
+  storageBucket: "chat-93135.appspot.com",
+  messagingSenderId: "499376383344",
+  appId: "1:499376383344:web:ab7ef5aa0fef1e0ea99bb5",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 import "./style.scss";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Umery Chats</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Enter your name" />
           <input type="email" placeholder="Enter your e-mail" />
           <input type="password" placeholder="Enter your password" />

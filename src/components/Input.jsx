d9449506106c5a5b5f0f892ba7ed386b6e42da29
umeree <umernadeam@gmail.com>
2023-08-13 14:07:36 +0500

    Design completed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Input = () => {
-  return <div className="input">Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img
+          src="https://res.cloudinary.com/dn5p6qvxn/image/upload/v1691901649/attach_haungp.png"
+          alt=""
+        />
+
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img
+            src="https://res.cloudinary.com/dn5p6qvxn/image/upload/v1691901649/img_sjrnws.png"
+            alt=""
+          />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/17884240/pexels-photo-17884240/free-photo-of-man-people-industry-business.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/17884240/pexels-photo-17884240/free-photo-of-man-people-industry-business.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;

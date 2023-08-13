b42364e717b1a5ea6909dc661206f70ca24c88b0
umeree <umernadeam@gmail.com>
2023-08-13 13:30:53 +0500

    Interface almost done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Umer</span>
+        <div className="chatIcons">
+          <img
+            src="https://res.cloudinary.com/dn5p6qvxn/image/upload/v1691901649/cam_fowhjl.png"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dn5p6qvxn/image/upload/v1691901649/add_hsmxty.png"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dn5p6qvxn/image/upload/v1691901649/more_njtoyj.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 };
 
 export default Chat;
---
+++ src/components/Chats.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/17884240/pexels-photo-17884240/free-photo-of-man-people-industry-business.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Umer</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/17884240/pexels-photo-17884240/free-photo-of-man-people-industry-business.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Umer</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/17884240/pexels-photo-17884240/free-photo-of-man-people-industry-business.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Umer</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;

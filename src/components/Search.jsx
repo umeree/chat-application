42da387197d2e1e8152a12105a12ddf451f67915
umeree <umernadeam@gmail.com>
2023-08-14 16:59:37 +0500

    Search module dynamic compltete

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,49 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { db } from "../firebase";
+import { collection, where, query, getDocs } from "firebase/firestore";
 const Search = () => {
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(null);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", userName)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user" />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/17884240/pexels-photo-17884240/free-photo-of-man-people-industry-business.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUserName(e.target.value)}
         />
-        <div className="userChatInfo">
-          <span>Umer</span>
-        </div>
       </div>
+      {err & <span>User not found</span>}
+      {user && (
+        <div className="userChat">
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
